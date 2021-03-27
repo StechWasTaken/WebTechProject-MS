7916275b9b633ff678ec4afb660772b2a50cc90e
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2021-03-27 15:44:24 +0100

    app.py veranderingen

--- app.py
+++ app.py
@@ -6,7 +6,3 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-@app.route('/')
-def index():
-    return render_template('home.html')
