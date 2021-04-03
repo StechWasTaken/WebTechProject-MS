d2749753f4078306ebf0233a91883765fac4e569
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2021-04-03 14:24:57 +0200

    versie terug

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import os
 from project import app
 from flask import Flask, render_template, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from project.model import db
+# from project.model import db
 
 # app = Flask(__name__)
 
 # app.register_blueprint(bp_docent)
 # https://flask.palletsprojects.com/en/1.1.x/blueprints/
 
-db.init_app(app)
+# db.init_app(app)
 
-Migrate(app, db)
+# Migrate(app, db)
 
 @app.route('/')
 def index():
