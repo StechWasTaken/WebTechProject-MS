0ec923c0c6f1370e9993e6504ebd1695fa126345
Pingwing0 <80771739+Pingwing0@users.noreply.github.com>
2021-04-11 20:09:10 +0200

    carousel afbeelding

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def index():
         role = getRole(current_user.id)
         name = current_user.username
 
+    # voor de cursus carousel
     cursussen = Course.query\
                 .filter_by()\
                 .join(Language, Course.language_id == Language.id)\
-                .add_columns(Language.language, Course.description).all()
+                .add_columns(Language.language, Course.description, Course.id).all()
 
     return render_template('home.html', role = role, name = name, cursussen=cursussen)
 
